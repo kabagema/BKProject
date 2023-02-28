f0dec09a907daa97bf812054f0b27b7c4c479c2e
kabagema <35719135+kabagema@users.noreply.github.com>
2023-02-28 05:42:14 +0200

    testiing component for a single item in the list

--- App.js
+++ App.js
@@ -139,46 +139,31 @@ export default App;
 
 */
 
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen({ navigation }) {
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import ProductItem from './screens/ProductList'
+export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+
+    <View>
+        <Text style={[styles.container]}>Product LIST</Text>
+        <ProductItem />
     </View>
+    
   );
 }
 
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+  heading: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 50,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+});
---
+++ screens/ProductList/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default ProductItem = (props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.indexContainer}>
+                <Text style={styles.index}> Product </Text>
+            </View>
+            
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+   
+    },
+    indexContainer: {
+        
+    },
+    index: {
+       
+    },
+    productContainer: {
+     
+    },
+    product: {
+       
+    },
+});
